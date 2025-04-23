2a8e6491325196ba8cb8f759a41d1c751096198a
K-MountainBook <keita.yamamoto.1009@gmail.com>
2025-04-23 16:35:36 +0900

    202504231635

---
+++ Definition.h
@@ -0,0 +1,22 @@
+#pragma once
+
+
+#include <DxLib.h>
+#include <string>
+
+#define WINDOW_WIDTH_HD		(1280)
+#define WINDOW_HEIGHT_HD		(720)
+#define WINDOW_WIDTH_FHD	(1920)
+#define WINDOW_HEIGHT_FHD	(1080)
+
+#define FPS_60				(60)
+
+//	êF
+const unsigned int red = GetColor(255, 0, 0);
+const unsigned int green = GetColor(0, 255, 0);
+const unsigned int blue = GetColor(0, 0, 255);
+const unsigned int magenta = GetColor(255, 0, 255);
+const unsigned int cyan = GetColor(0, 255, 255);
+const unsigned int yellow = GetColor(255, 255, 0);
+const unsigned int white = GetColor(255, 255, 255);
+const unsigned int black = GetColor(0, 0, 0);
---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include "DxLib.h"
+#include "Definition.h"
+
+// プログラムは WinMain から始まります
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+
+
+	SetGraphMode(WINDOW_WIDTH_HD, WINDOW_HEIGHT_HD, 32, FPS_60);
+	ChangeWindowMode(TRUE);
+
+	if (DxLib_Init() == -1)		// ＤＸライブラリ初期化処理
+	{
+		return -1;			// エラーが起きたら直ちに終了
+	}
+
+	// 画面を描画する先をバックバッファに設定する→バックバッファに全て書き出してからフロントバッファにデータを送信するため
+	SetDrawScreen(DX_SCREEN_BACK);
+
+	// フレーム数調整用
+	int time;
+
+	// メインループ
+	while (1) {
+
+		// ミリ秒単位の現在時刻を得る
+		time = GetNowCount();
+
+
+		// バックバッファの書き込みが終わったらフロントバッファへデータを転送する。
+		ScreenFlip();
+
+		// このループの処理が指定秒間フレームの処理時間より早く終わった場合は処理を待つ
+		while (1) {
+			if (GetNowCount() - time >= 1000 / FPS_60) {
+				break;
+			}
+		}
+
+	}
+
+	DxLib_End();				// ＤＸライブラリ使用の終了処理
+
+	return 0;				// ソフトの終了 
+}
