627c55034f4250935ab51103d06fdf52b263c081
K-MountainBook <keita.yamamoto.1009@gmail.com>
2025-04-23 16:45:40 +0900

    202504231645

--- Definition.h
+++ Definition.h
@@ -20,3 +20,41 @@ const unsigned int cyan = GetColor(0, 255, 255);
 const unsigned int yellow = GetColor(255, 255, 0);
 const unsigned int white = GetColor(255, 255, 255);
 const unsigned int black = GetColor(0, 0, 0);
+
+/// <summary>
+/// ゲームオブジェクト構造体（これをベースとする）
+/// </summary>
+struct GameObject {
+	bool isVisible;					// 表示・非表示
+	int image;						// 画像ハンドラ
+	int width, height;				// サイズ
+	float x, y;						// 座標
+	float cx, cy;					// 中心座標
+	float radius;					// 半径
+
+	void Init(int _image, bool _visible = true, float _x = 0.0f, float _y = 0.0f, float _radius = 0.0f) {
+		// 表示フラグの初期化
+		isVisible = _visible;
+		// 画像ハンドルの初期化
+		image = _image;
+		// 画像サイズの初期化
+		GetGraphSize(image, &width, &height);
+		// 座標の初期化
+		x = _x;
+		y = _y;
+		// 中央座標の初期化（指定された座標＋イメージのサイズの半分（中央）
+		cx = _x + width * 0.5f;
+		cx = _y + height * 0.5f;
+		// 半径の初期化
+		radius = _radius;
+	}
+};
+
+struct Player {
+	GameObject gameObject;
+	float moveX, moveY;
+
+	void Init(int _image, bool _visible = true, float _x = 0.0f, float _y = 0.0f, float _radius = 0.0f) {
+
+	}
+};
